4ce2eae181667322e453cb21ca22f0b1748c9bba
Raphael Fialho <rapguit@gmail.com>
2018-06-13 04:58:49 -0300

    dockering

--- src/main/java/com/books/bookshelf/BookshelfApplication.java
+++ src/main/java/com/books/bookshelf/BookshelfApplication.java
@@ -2,11 +2,18 @@ package com.books.bookshelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class BookshelfApplication {
+public class BookshelfApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookshelfApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookshelfApplication.class, args);
